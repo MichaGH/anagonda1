fc290575a207d227a045985467ce25a220821334
MichaGH <74835635+MichaGH@users.noreply.github.com>
2025-08-04 00:42:21 +0200

    fixed

--- app.js
+++ app.js
@@ -1,22 +1,28 @@
 const express = require('express');
 const app = express();
-/*
-- handle routing (GET, POST, PUT...)
-- server static files
-- parse json
-- middleware
-
-*/
-
 
 // set public folder
 app.use( express.static('public') )
 
-// view engine
+// set view engine and folder with views
 app.set('view engine', 'ejs')
 app.set('views', './views')
 
 
+// ROUTING
+/*
+    app.TypRequestu(adresa, (req, res) => {
+        
+        Co sa ma stat po requeste
+        vsetko o requeste je v req objekte (request)
+        vsetko co chceme poslat naspat davame do res objekut (response)
+        Naspat uzivatelovi posielame 
+
+        res.render('Lokacia view ktory chceme rendernut', {dynamicke data})
+        
+        })
+
+*/
 
 
 app.get('/', (req, res) => {
@@ -26,15 +32,18 @@ app.get('/', (req, res) => {
 app.get('/about', (req, res) => {
     res.render('about', {title: 'About Me'})
 })
+
 app.get('/contact', (req, res) => {
     res.render('contact', {title: 'Contact Me'})
 })
 
 
 
-// localhost:3000
-app.listen(3000, () => {
-    console.log('App is running on localhost!')
+/* Zapnut Aplikaciu na localhost */
+
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log(`App is running on http://localhost:${PORT}`)
 })
 
 
